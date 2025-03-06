ccb45af2a3fbb35d5a131430c92d27852d222656
Yong Tang <yong.tang.github@outlook.com>
2025-03-06 03:37:49 +0000

    Print loop

--- motion_extension/motion_extension.py
+++ motion_extension/motion_extension.py
@@ -1,9 +1,20 @@
+import asyncio
 import omni.ext
 
 
 class MotionExtension(omni.ext.IExt):
     def on_startup(self, ext_id):
         print("[MotionExtension] Extension Startup")
+        self._running = True
+        self._task = asyncio.ensure_future(self.hello_world_loop())
 
     def on_shutdown(self):
         print("[MotionExtension] Extension Shut Down")
+        self._running = False
+        if self._task:
+            self._task.cancel()
+
+    async def hello_world_loop(self):
+        while self._running:
+            print("[MotionExtension] Hello, World!")
+            await asyncio.sleep(1)
